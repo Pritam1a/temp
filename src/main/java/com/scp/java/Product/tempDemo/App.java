6ad549fdde4420a4f57a060e509cc99d37096dcf
pritam1a <pritam1a@gmail.com>
2018-12-10 15:38:13 +0530

    first commit

---
+++ src/main/java/com/scp/java/Product/tempDemo/App.java
@@ -0,0 +1,13 @@
+package com.scp.java.Product.tempDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/scp/java/Product/tempDemo/DemoClass.java
@@ -0,0 +1,11 @@
+package com.scp.java.Product.tempDemo;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Hello");
+	}
+
+}
